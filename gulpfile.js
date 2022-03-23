9217abc4604743928ca88be0a7c8a0f698b02fa8
kakhi-varazashvili <kakhi_varazashvili@yahoo.it>
2022-03-24 03:40:19 +0400

    first section

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass') (require ('node-sass'));
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('styles'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('scss/**/*.scss', gulp.parallel ('sass'));
+});
